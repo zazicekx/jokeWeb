6e56fa4bfe5763f52396aa3557b2b37663642289
Zazicekx <zeinert@glancemedia.local>
2023-08-10 15:15:40 +0200

    added meme, testing on phone

--- src/pages/WelcomePage.js
+++ src/pages/WelcomePage.js
@@ -5,10 +5,13 @@ import "../CSS/jokebutton.css"
 const WelcomePage = () => {
   const [joke, setJoke] = useState('');
   const [joke1, setJoke1] = useState('');
+  const [joke2, setJoke2] = useState('');
   const [isVisible, setIsVisible] = useState(false);
   const [isVisible1, setIsVisible1] = useState(false);
+  const [isVisible2, setIsVisible2] = useState(false);
   const [timerId, setTimerId] = useState(null);
   const [timerId1, setTimerId1] = useState(null);
+  const [timerId2, setTimerId2] = useState(null);
 
   const startTimer = (setVisibleFunction) => {
     return setTimeout(() => {
@@ -17,6 +20,12 @@ const WelcomePage = () => {
   };
 
   const startTimer1 = (setVisibleFunction) => {
+    return setTimeout(() => {
+      setVisibleFunction(false);
+    }, 8000);
+  };
+
+  const startTimer2 = (setVisibleFunction) => {
     return setTimeout(() => {
       setVisibleFunction(false);
     }, 8000);
@@ -64,6 +73,27 @@ const WelcomePage = () => {
     }
   };
 
+  const fetchJoke2 = async () => {
+    try {
+      const response = await fetch('https://meme-api.com/gimme', {
+        headers: {
+          Accept: 'application/json',
+        },
+      });
+      const data = await response.json();
+      setJoke2(data.url);
+      setIsVisible2(true);
+
+
+      clearTimeout(timerId2); // Clear the previous timer
+      const newTimerId = startTimer2(setIsVisible2); // Start a new timer
+      setTimerId2(newTimerId);
+
+    } catch (error) {
+      console.error('Error fetching joke:', error);
+    }
+  };
+
  
   return (
     <div className="jokeButtonContainer">
@@ -71,6 +101,8 @@ const WelcomePage = () => {
       {isVisible && <p>{joke}</p>}
       <button onClick={fetchJoke1} className="jokeButton">Get Joke</button>
       {isVisible1 && <p>{joke1}</p>}
+      <button onClick={fetchJoke2} className="jokeButton">Get MeMe</button>
+      {isVisible2 && <img src={joke2} alt="Logo" />}
     </div>
   );
 };
